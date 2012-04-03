027021b459dafb667daf4f5f88b07b5a40037c57
Consti <consti37@googlemail.com>
2012-04-03 13:43:32 +0200

    Rotationsbehandlung für die Account Erstellen Activity überarbeitet

--- src/org/alarmapp/activities/account_create/AccountCreateWebViewClient.java
+++ src/org/alarmapp/activities/account_create/AccountCreateWebViewClient.java
@@ -17,7 +17,6 @@
 package org.alarmapp.activities.account_create;
 
 import org.alarmapp.AlarmApp;
-import org.alarmapp.util.ActivityUtil;
 import org.alarmapp.util.IntentUtil;
 
 import android.webkit.WebView;
@@ -31,12 +30,6 @@ public class AccountCreateWebViewClient extends android.webkit.WebViewClient {
 		}
 		view.loadUrl(url);
 		return true;
-	}
-
-	@Override
-	public void onPageFinished(WebView view, String url) {
-		super.onPageFinished(view, url);
-		ActivityUtil.stopProgressBar();
 	}
 
 	private boolean isRegistrationDone(String url) {
